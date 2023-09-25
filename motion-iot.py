6aee97c6100c4cfc167de24edf8cd2f775948434
Usama Alian <engalian@gmail.com>
2023-09-24 20:17:49 -0500

    pir with led,buzzer and relay

--- motion-iot.py
+++ motion-iot.py
@@ -1,14 +1,19 @@
+# this code will detect motion using PIR sensor on pin 18 anf then set other GPIO's to 1 such as LED, Buzzer and relay
+# i connected the relay to a car motor operated by battery and worked well.
 import RPi.GPIO as io
 import time
 io.setmode(io.BCM)
 io.setmode(io.BCM)
 
 pir_sensor = 18
-piezo = 27
-st17=17
-
+piezo = 24
+st17=22
+bLed=27
+relay=21
+io.setup(relay,io.OUT)
 io.setup(piezo,io.OUT)
 io.setup(st17,io.OUT)
+io.setup(bLed,io.OUT)
 io.setup(pir_sensor,io.IN)
 current_state = 0
 io.output(st17,current_state)
@@ -20,11 +25,13 @@ try:
 #Read the Current_State of the Sensor and immediately set the GPIOs to that val$
         io.output(piezo,current_state)
         io.output(st17,current_state)
-        time.sleep(0.5)
+        io.output(bLed,current_state)
+        io.output(relay,current_state)
+        time.sleep(5.5)
         io.output(piezo,False)
 #Provide verbose output on the states if needed
         if current_state == 1:
-            print("MOTION ALARM ACTIVATED GPIO pin %s is %s" % (pir_sensor, current_state))
+            print("MOTION ALARM ACTIVATED GPIO pin %s is %s" % (pir_sensor, curr                                                                                        ent_state))
             time.sleep(1)
         else:      #current_state==0:
             io.output(piezo,False)
@@ -38,6 +45,8 @@ except KeyboardInterrupt:
 
     pass
     io.output(st17,0)
+    io.output(bLed,0)
+    io.output(relay,0)
 finally:
     io.output(st17,0)
     io.cleanup()
